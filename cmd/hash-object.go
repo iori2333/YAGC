718413d62f31d4d2f34cfdb973dc1ca6f33d3923
Iori Ichinose <milk.ichinose.iori@gmail.com>
2022-02-03 16:56:21 +0800

    feat: support hashing TreeObject

--- cmd/hash-object.go
+++ cmd/hash-object.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"log"
+	"os"
 
 	"yagc/db"
 	"yagc/models"
@@ -11,25 +12,35 @@ import (
 
 func handleHashObject(cmd *cobra.Command, args []string) {
 	if len(args) == 0 {
-		log.Fatalln("missing argument: file")
+		log.Fatalln("Missing argument: file")
 	}
 
-	t := cmd.Flag("type").Value.String()
-	if t != "blob" {
-		log.Fatalln("unsupported object type:", t)
-	}
+	file := args[0]
+	var obj models.Object
 
-	write := cmd.Flag("write").Value.String() == "true"
-
-	obj := models.BlobObject{
-		File: args[0],
+	switch t := cmd.Flag("type").Value.String(); t {
+	case "blob":
+		obj = &models.BlobObject{
+			File: file,
+		}
+	case "tree":
+		tree := models.TreeObject{}
+		content, err := os.ReadFile(file)
+		if err != nil {
+			log.Fatalf("Failed to read file %s: %s\n", file, err)
+		}
+		obj = tree.Parse(content)
+	case "commit":
+		log.Fatalln("Not implemented")
+	default:
+		log.Fatalf("Invalid type received: %s\n", t)
 	}
 
 	sha1, content := obj.GetSha1()
 	log.Println(sha1)
 
-	if write {
-		db.Write(sha1, content)
+	if cmd.Flag("write").Changed {
+		db.WriteObject(sha1, content)
 	}
 }
 
@@ -37,14 +48,15 @@ func getHashObjectCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "hash-object file",
 		Short: "Compute object ID and optionally creates a blob from a file",
-		Long:  `Computes the object ID value for an object with specified type with the contents of the named file (which can be outside of the work tree), and optionally writes the resulting object into the object database. Reports its object ID to its standard output. When <type> is not specified, it defaults to "blob".`,
-		Run:   handleHashObject,
+		Long: `Computes the object ID value for an object with specified type with the contents of the named file
+(which can be outside of the work tree), and optionally writes the resulting object into the object database.
+Reports its object ID to its standard output. When <type> is not specified, it defaults to "blob".`,
+		Run:    handleHashObject,
+		Hidden: true,
 	}
 
 	cmd.Flags().StringP("type", "t", "blob", "Specify the type of the object")
-
 	cmd.Flags().BoolP("write", "w", false, "Actually write the object into the database")
-
 	cmd.Flags().Bool("stdin", false, "Read standard input instead of a file")
 
 	return cmd
